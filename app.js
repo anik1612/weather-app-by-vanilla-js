dc0eb121d4284b560e5ba61e1e0963e67edf0cc1
Anik Sarker <47253023+anik1612@users.noreply.github.com>
2020-08-10 23:27:16 +0800

    Update app.js

--- app.js
+++ app.js
@@ -9,8 +9,6 @@ async function getCity(){
 }
 
 getCity().then(data => {
-    const country = document.getElementById('w-country');
-    country.TextContent = data.country;
     const weather = new Weather(data.city);
     weather.getWeather().then(data => {
         ui.displayWeatherInfo(data); 
